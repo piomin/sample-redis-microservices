2fa24a6f2886752a7cc3bdb8d0ce94f78b4d0217
piomin <piotr.minkowski@gmail.com>
2023-02-27 00:51:52 +0100

    fix tests

--- driver-management/src/test/java/pl/piomin/services/driver/DriverRepositoryIntegrationTests.java
+++ driver-management/src/test/java/pl/piomin/services/driver/DriverRepositoryIntegrationTests.java
@@ -1,7 +1,11 @@
 package pl.piomin.services.driver;
 
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 import pl.piomin.services.driver.model.Driver;
 import pl.piomin.services.driver.model.DriverStatus;
 import pl.piomin.services.driver.repository.DriverRepository;
@@ -11,9 +15,25 @@ import org.springframework.boot.test.autoconfigure.data.redis.DataRedisTest;
 import org.springframework.data.geo.Point;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+//@RunWith(SpringRunner.class)
 @DataRedisTest
+@Testcontainers
 public class DriverRepositoryIntegrationTests {
+
+	@Container
+	static final GenericContainer redis = new GenericContainer("redis:latest")
+			.withExposedPorts(6379);
+
+//	@DynamicPropertySource
+//	static void redisProperties(DynamicPropertyRegistry registry) {
+//		int port = redis.getFirstMappedPort();
+//		registry.add("spring.redis.port", () -> port);
+//	}
+
+	@BeforeAll
+	static void init() {
+		System.setProperty("spring.redis.port", redis.getFirstMappedPort().toString());
+	}
 
 	@Autowired
 	DriverRepository repository;
