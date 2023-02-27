54938f73cc3d4faa181769168302a320d06c2472
piomin <piotr.minkowski@gmail.com>
2023-02-27 01:06:50 +0100

    fix tests (3)

--- trip-management/src/test/java/pl/piomin/services/trip/TripControllerIntegrationTests.java
+++ trip-management/src/test/java/pl/piomin/services/trip/TripControllerIntegrationTests.java
@@ -1,20 +1,17 @@
 package pl.piomin.services.trip;
 
-import java.util.Date;
-
-import org.junit.Test;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.util.Assert;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import pl.piomin.services.trip.model.Trip;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.util.Assert;
+import java.util.Date;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Testcontainers
